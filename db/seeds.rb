fdc6307042c5aea555f76dfcfbf1bf663c3e3ff9
ɐʇɹɐʎpıʍ ɥı|ɐƃ <hashirama@Galihs-MacBook-Air.local>
2015-01-25 20:00:40 +0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Task.destroy_all
+Comment.destroy_all
+
+xavier = User.create first_name: 'Charles', last_name: 'Xavier', email: 'xavier@xmen.com', password: 'secret1234'
+
+wolverine = User.create first_name: 'Wolverine', email: 'wolverine@xmen.com', password: 'secret1234'
+cyclops = User.create first_name: 'Cyclops', email: 'cyclops@xmen.com', password: 'secret1234'
+beast = User.create first_name: 'Beast', email: 'beast@xmen.com', password: 'secret1234'
+
+wolverine_task = xavier.task_owners.create name: 'Kidnap Magneto', description: 'Coordinate with beast to kidnap magneto', user: wolverine
+beast_task = xavier.task_owners.create name: 'Hack Magneto Server', description: 'Coordinate with wolverine to hack magneto server', user: beast
+cyclops_task = xavier.task_owners.create name: 'Destroy Magneto Headquarter', description: 'Coordinate with Wolverine and Beast to destroy Magneto Headquarter', user: beast
+
+wolverine_task.comments.create body: 'wakarimasu', user: wolverine
+beast_task.comments.create body: 'roger that!', user: beast
+cyclops_task.comments.create body: '86!', user: cyclops
+
