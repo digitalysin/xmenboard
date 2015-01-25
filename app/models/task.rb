15cc5258af2211d2fcffaa46ff616df7344d9496
ɐʇɹɐʎpıʍ ɥı|ɐƃ <hashirama@Galihs-MacBook-Air.local>
2015-01-25 22:20:31 +0700

    update task spec

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,17 @@ class Task < ActiveRecord::Base
   belongs_to :user
   belongs_to :owner, class_name: 'User'
   has_many :comments
+
+  validates :name, :description, presence: true
+
+  before_save :before_save_callback
+
+  private
+  def before_save_callback
+    if finished?
+      self.finished_at = DateTime.now
+    else
+      self.finished_at = nil
+    end
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -11,4 +11,21 @@ describe Task do
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :owner }
   it { is_expected.to have_many :comments }
+
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :description }
+
+  context 'before_save' do
+    let(:task) { create :task, finished: false, finished_at: 1.day.ago }
+    subject { task }
+
+    context 'when finished attributes is set to false' do
+      its(:finished_at) { is_expected.to be_blank }
+    end
+
+    context 'when finished is set to true' do
+      before { task.update_attributes(finished: true) }
+      its(:finished) { is_expected.to be_present }
+    end
+  end
 end
